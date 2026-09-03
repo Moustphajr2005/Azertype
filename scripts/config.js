8020f2a113e2facaa518dcb7d195c95415ea612e
Moustphajr2005 <moustaphakhouma2005@gmail.com>
2026-09-03 13:45:47 +0000

    Premier Commit:Jeu de mots et phrases

---
+++ scripts/config.js
@@ -0,0 +1,3 @@
+// Déclaration du tableau contenant la liste des mots proposés à l'utilisateur
+const listeMots = ['Cachalot', 'Pétunia', 'Serviette']
+const listePhrases=["La cavalerie est là","Je vais bien","Entend ma rage"]
